d679c8bf75a5551d204b24af2a29ddc629bd1b88
chasereckling <chasereckling@gmail.com>
2023-09-26 12:16:34 -0700

    add try to function passed to main

--- exceptions/number_try_except_valueerror.py
+++ exceptions/number_try_except_valueerror.py
@@ -1,8 +1,26 @@
-try:
-    # passing the return value of input as the argument to int function and x will store the value
-    x = int(input("what's x? "))
-    # use f 'format string' to interpolate the value of the x variable. otherwise python will just print {x} literally
-except ValueError:
-    print("x is not an integer")
-else:
+# w/o function
+# try:
+#     # passing the return value of input as the argument to int function and x will store the value
+#     x = int(input("what's x? "))
+#     # use f 'format string' to interpolate the value of the x variable. otherwise python will just print {x} literally
+# except ValueError:
+#     print("x is not an integer")
+# else:
+#     print(f"x is {x}")
+
+
+# w/ function
+def main():
+    x = get_int()
     print(f"x is {x}")
+
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            pass
+
+
+main()

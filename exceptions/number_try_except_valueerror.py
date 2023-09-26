1ae0c0a83f32d0482c9581d3402c75faff79f966
Chase Reckling <chasereckling@gmail.com>
2023-09-26 13:01:00 -0700

    end exceptions section (#2)
    * add try to function passed to main
    
    * add parameter to get_int function for function caller prompt extendability

--- exceptions/number_try_except_valueerror.py
+++ exceptions/number_try_except_valueerror.py
@@ -1,8 +1,26 @@
-try:
-    # passing the return value of input as the argument to int function and x will store the value
-    x = int(input("what's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
-    # use f 'format string' to interpolate the value of the x variable. otherwise python will just print {x} literally
+# w/o function
+# try:
+#     # passing the return value of input as the argument to int function and x will store the value
+#     x = int(input("what's x? "))
+#     # use f 'format string' to interpolate the value of the x variable. otherwise python will just print {x} literally
+# except ValueError:
+#     print("x is not an integer")
+# else:
+#     print(f"x is {x}")
+
+
+# w/ function
+def main():
+    x = get_int("What's x? ")
     print(f"x is {x}")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+
+main()

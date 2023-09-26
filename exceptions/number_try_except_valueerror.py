690610808f7ea4ba8bec2e27f24418661cca1bee
Chase Reckling <chasereckling@gmail.com>
2023-09-26 11:41:38 -0700

    add print function to try else statement for error handling (#1)

--- exceptions/number_try_except_valueerror.py
+++ exceptions/number_try_except_valueerror.py
@@ -1,8 +1,8 @@
 try:
     # passing the return value of input as the argument to int function and x will store the value
     x = int(input("what's x? "))
-    # use f 'format string' to interpolate the value of the x variable. otherwise python will just print {x} literally
 except ValueError:
     print("x is not an integer")
 else:
+    # use f 'format string' to interpolate the value of the x variable. otherwise python will just print {x} literally
     print(f"x is {x}")
